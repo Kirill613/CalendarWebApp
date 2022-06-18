da3e20eeac60cf36ab4cbc0caeb898558617bbdb
Kirill613 <kirillshcherbakov2002@gmail.com>
2022-06-18 23:33:53 +0300

    add calendar microservice

---
+++ CalendarAPI.Services/Controllers/CalendarController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalendarAPI.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+        // GET: api/<CalendarController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "calendar1", "calendar2" };
+        }
+
+        // GET api/<CalendarController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "calendar";
+        }
+
+        // POST api/<CalendarController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CalendarController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CalendarController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
